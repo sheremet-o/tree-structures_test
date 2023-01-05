b5812351e9eb38df96b803eca9aea0dd5bdedf99
Oksana Sheremet <sheremet5631@gmail.com>
2023-01-05 14:58:02 +0300

    added models

--- menu_generator/menus/admin.py
+++ menu_generator/menus/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu, MenuItem
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'created_at',
+        'position'
+    )
+
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'menu',
+    )
+
+
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(MenuItem, MenuItemAdmin)
--- menu_generator/menus/models.py
+++ menu_generator/menus/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+import mptt
+
+
+class Menu(models.Model):
+    name = models.CharField(
+        max_length=150,
+        unique=True,
+        blank=False,
+        verbose_name='Название'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата добавления'
+    )
+    position = models.CharField(
+        max_length=100,
+        blank=False,
+        verbose_name='Позиция'
+    )
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(
+        Menu,
+        verbose_name='Меню',
+        on_delete=models.CASCADE,
+    )
+    parent = models.ForeignKey(
+        'self',
+        related_name=u'child',
+        blank=True,
+        null=True,
+        on_delete=models.DO_NOTHING
+    )
+    name = models.CharField(
+        max_length=150,
+        null=False,
+        verbose_name='Пункт меню'
+    )
+
+    class Meta:
+        verbose_name = 'Пункт меню'
+        verbose_name_plural = 'Пункты меню'
+
+    def __str__(self):
+        return self.name
+
+
+mptt.register(MenuItem,)
