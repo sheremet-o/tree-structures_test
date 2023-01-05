45cbe5e2a7a39f8c67761c33c237c7f784eb3034
Oksana Sheremet <sheremet5631@gmail.com>
2023-01-05 18:09:50 +0300

    added urls, static

--- menu_generator/menu_generator/settings.py
+++ menu_generator/menu_generator/settings.py
@@ -89,8 +89,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS =(
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
