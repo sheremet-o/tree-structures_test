b0e215225245ec0524f5842d0b26a89cc0d5c628
Oksana Sheremet <sheremet5631@gmail.com>
2023-01-05 17:16:07 +0300

    fixed models, added views

--- menu_generator/menus/admin.py
+++ menu_generator/menus/admin.py
@@ -1,24 +1,13 @@
 from django.contrib import admin
 
-from .models import Menu, MenuItem
-
-
-class MenuAdmin(admin.ModelAdmin):
-    list_display = (
-        'pk',
-        'name',
-        'created_at',
-        'position'
-    )
+from .models import MenuItem
 
 
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
         'name',
-        'menu',
     )
 
 
-admin.site.register(Menu, MenuAdmin)
 admin.site.register(MenuItem, MenuItemAdmin)
--- menu_generator/menus/models.py
+++ menu_generator/menus/models.py
@@ -1,51 +1,23 @@
 from django.db import models
 
-import mptt
-
-
-class Menu(models.Model):
-    name = models.CharField(
-        max_length=150,
-        unique=True,
-        blank=False,
-        verbose_name='Название'
-    )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        db_index=True,
-        verbose_name='Дата добавления'
-    )
-    position = models.CharField(
-        max_length=100,
-        blank=False,
-        verbose_name='Позиция'
-    )
-
-    class Meta:
-        verbose_name = 'Меню'
-        verbose_name_plural = 'Меню'
-
-    def __str__(self):
-        return self.name
-
 
 class MenuItem(models.Model):
-    menu = models.ForeignKey(
-        Menu,
-        verbose_name='Меню',
-        on_delete=models.CASCADE,
+    name = models.CharField(
+        max_length=150,
+        blank=False,
+        verbose_name='Пункт меню'
     )
     parent = models.ForeignKey(
         'self',
-        related_name=u'child',
         blank=True,
         null=True,
-        on_delete=models.DO_NOTHING
+        on_delete=models.CASCADE
     )
-    name = models.CharField(
+    url = models.URLField(
         max_length=150,
-        null=False,
-        verbose_name='Пункт меню'
+        blank=True,
+        null=True,
+        unique=True
     )
 
     class Meta:
@@ -55,5 +27,10 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.name
 
+    def children(self):
+        return self.menuitem_set.all()
 
-mptt.register(MenuItem,)
+    def get_parents(self):
+        if not self.parent:
+            return []
+        return self.parent.get_parents() + [self.parent.id]
